b20bfad7050c9b1c6ead6a29cfbe60dc898abb3d
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-10-09 07:39:20 +0530

    updated

--- src/Componnet/Header/Header.js
+++ src/Componnet/Header/Header.js
@@ -3,7 +3,20 @@ import classes from './Header.module.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={classes.Header}>
+      <div className={classes.LeftHeader}> 
+         <img src="" alt="" />
+         <p>Phones</p>
+         <p>Tv's</p>
+      </div>
+      <div className={classes.RightHeader}>
+        <input type="text" />
+        {/* search box Logo */}
+        <p className={classes.Signup}>Sign Up</p>
+        {/* Basket logo */}
+        <span className={classes.ItemCount}>0</span>
+      </div>
+    </div>
   )
 }
 
